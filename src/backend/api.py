63b4bd803345761bcff5c43f0e0757b0a242878a
Radhika <radhika.80599@gmail.com>
2018-12-30 20:57:16 +0530

    Added /login and /logout in api.py (MySQL and Flask connection)

--- src/backend/api.py
+++ src/backend/api.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
-from flask import Flask, request, jsonify, render_template, abort
+from flask import Flask, request, jsonify, render_template, session, redirect, url_for, excape, abort
 from flask_cors import CORS, cross_origin
 from .db_handler import DBHandler
 from .predict import *
@@ -10,6 +10,7 @@ import os
 import random
 import datetime
 import pandas as pd
+import MySQLdb
 
 CASUALTY_DATA_FILE = "data/random_facts.json"
 
@@ -17,6 +18,8 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+db = MySQLdb.connect(host="35.200.235.106", user="root", passwd="password77", db="messiah")
+cur = db.cursor()
 
 @app.route('/history', methods=['GET'])
 @cross_origin()
@@ -145,4 +148,43 @@ def signup():
 
     except:
         return jsonify({'success': False})
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        username_session = escape(session['username']).capitalize()
+        return render_template('index.html', session_user_name=username_session)
+    return redirect(url_for('login'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if 'username' in session:
+        return redirect(url_for('index'))
+    if request.method == 'POST':
+        username_form  = request.form['username']
+        password_form  = request.form['password']
+        cur.execute("SELECT COUNT(1) FROM users WHERE Name = %s;", [username_form]) # CHECKS IF USERNAME EXSIST
+        if cur.fetchone()[0]:
+            cur.execute("SELECT password FROM users WHERE Name = %s;", [username_form]) # FETCH THE HASHED PASSWORD
+            for row in cur.fetchall():
+                if password_form.hexdigest() == row[0]:
+                    session['username'] = request.form['username']
+                    return redirect(url_for('index'))
+                else:
+                    error = "Invalid Credential"
+        else:
+            error = "Invalid Credential"
+    return render_template('Login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
